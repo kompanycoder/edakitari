230fb9bae1a584f535f86f1fba9a9395b04d80e8
KompanyCoder <mwegagideon@gmail.com>
2022-01-21 16:04:52 +0300

    Added few changes to layout and working on sign in process

--- app/src/main/java/com/example/edakitari/ui/user/SignIn.kt
+++ app/src/main/java/com/example/edakitari/ui/user/SignIn.kt
@@ -1,16 +1,29 @@
 package com.example.edakitari.ui.user
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.os.Parcel
 import android.os.Parcelable
+import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.example.edakitari.R
 
 class SignIn() : AppCompatActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//
-//    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign_in)
+        val googleSignInButton: Button = findViewById(R.id.google_sign_btn)
+        googleSignInButton.setOnClickListener { v ->
+            Log.d(TAG, "Google sign IN btn clicked: $v")
+        }
+    }
+    override fun onResume() {
+        super.onResume()
+//        check if user instance is loggedIn then push user to profile/ services
+
+    }
 //    Add google signIn button
 //    add logic for signing in user
 //    if user instance is present, push to profile or home
